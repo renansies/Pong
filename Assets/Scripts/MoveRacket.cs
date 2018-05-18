4bb736899cc9bb2debc43990b6a45bb406b777f3
Renan Sies <renansies@gmail.com>
2018-05-18 12:04:54 -0300

    Movimentos e adicionando a bola

---
+++ Assets/Scripts/MoveRacket.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour
+{
+
+	[SerializeField] private float speed = 30;
+	[SerializeField] private string axis = "Vertical";
+	
+	public void FixedUpdate ()
+	{
+		float v = Input.GetAxisRaw(axis);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+	}
+}
